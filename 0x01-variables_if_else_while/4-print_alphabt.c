3db5344616b77097d624f7bec93af13d9555797b
MarioRaafat <mariorafat10@gmail.com>
2023-07-14 19:10:57 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char xx;
+
+	for (xx = 'a'; xx <= 'z'; xx++)
+	{
+		if (xx != e & xx != q)
+		putchar(xx);
+	}
+	putchar('\n');
+	return (0);
+}

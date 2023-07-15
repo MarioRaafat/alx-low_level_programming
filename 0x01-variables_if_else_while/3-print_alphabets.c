3365b02b7279a471bf00d11a3a59abd6f68bed28
MarioRaafat <mariorafat10@gmail.com>
2023-07-14 18:59:36 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char xx;
+
+	for (xx = 'a'; xx <= 'z'; xx++)
+		putchar(xx);
+	putchar('\n');
+	for (xx = 'A'; xx <= 'Z'; xx++)
+	return (0);
+}

d09e5c4631f57507f7cc6c61b057210d0a5e0809
MarioRaafat <mariorafat10@gmail.com>
2023-07-14 19:44:58 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar('0' + num);
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}

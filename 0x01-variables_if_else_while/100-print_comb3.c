347ae648a3f943089e6426fdea52e42a29a50dd1
MarioRaafat <mariorafat10@gmail.com>
2023-07-14 19:56:24 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int num, numm;
+
+	for (numm = 0; numm < 9; numm++)
+	for (num = numm + 1; num < 10; num++)
+	{
+		putchar('0' + num);
+		purchar('0' + numm);
+		if (numm != 8)
+		{
+		putchar(44);
+		putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

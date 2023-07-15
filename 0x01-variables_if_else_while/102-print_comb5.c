510cc303342e12b6e24cd6390ce9f7417bfc9f2d
MarioRaafat <mariorafat10@gmail.com>
2023-07-14 20:36:34 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int num, numm, x, xx;
+
+	for(x = 0; x < 10; xx++)
+	{
+		for(xx = 0; xx < 10; xx++)
+		{
+	for (numm = x; numm < 10; numm++)
+	{
+		for (num = xx + 1; num < 10; num++)
+		{
+			putchar('0' + numm);
+			putchar('0' + num);
+			if (x == 9 & xx == 9)
+				break;
+			if (x != 9 || xx != 8)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

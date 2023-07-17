30fa90d270f1e56b3aa81d3eec4312fa168c5586
MarioRaafat <mariorafat10@gmail.com>
2023-07-16 19:34:00 -0700

    advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1,quo,59);
+	return (1);
+}

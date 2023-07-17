fc44eadabbbfc4277a99d1de94417d788384187e
MarioRaafat <mariorafat10@gmail.com>
2023-07-16 18:52:57 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
-#include<stdio.h>
-int main()
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n,");
+	printf("\"with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }

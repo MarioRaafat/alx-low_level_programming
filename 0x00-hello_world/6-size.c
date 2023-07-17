cf001e7cf0749a759e1ce4019f9b3986265a7b71
MarioRaafat <mariorafat10@gmail.com>
2023-07-16 19:10:57 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	printf("size of a char:", sizeof(char));
+	printf("size of a int:", sizeof(int));
+	printf("size of a long int:", sizeof(long int));
+	printf("size of a long long:", sizeof(long long int));
+	printf("size of a float:", sizeof(float));
+	return (0);
+}

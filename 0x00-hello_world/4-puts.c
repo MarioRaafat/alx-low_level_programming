5ae0ea7676c95cd781e3bdeba167ec49601b8b9e
MarioRaafat <mariorafat10@gmail.com>
2023-07-16 18:03:03 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+int main(void) /*this is the start*/
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}

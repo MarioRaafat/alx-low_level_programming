8a6fd7357eacf388ca22524afcaa141a5cbb49ad
MarioRaafat <mariorafat10@gmail.com>
2023-07-22 14:46:17 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	printf("/_putchar");
+	return (0);
+}

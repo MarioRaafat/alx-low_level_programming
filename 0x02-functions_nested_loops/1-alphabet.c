b056c097563a3e8da39db5f0c10ceecdb3d184f5
MarioRaafat <mariorafat10@gmail.com>
2023-07-22 15:20:47 -0700

    small_alphapet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for(i = 97,i < 123);
+	_putchar(i);
+	_putchar('\n');
+	return(0);
+}

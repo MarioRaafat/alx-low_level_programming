cabd2ef7098fd533c197513006dd19aacc265c4e
MarioRaafat <mariorafat10@gmail.com>
2023-07-23 05:34:43 -0700

    mm

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,12 @@
 /**
  * print_alphapet - utilizeson the _putchar function to ptint the alphapet a - z
 */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	int ch;
 
 	for (ch = 'a'; ch < 'z'; ch++)
 	_putchar(ch);
 	_putchar('\n');
-	return (0);
+	return(0);
 }

100887c3430dd33ee518d66e4cb6f7737ada16ca
MarioRaafat <mariorafat10@gmail.com>
2023-07-23 05:41:50 -0700

    mm

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,14 @@
 #include "main.h"
-/**
- * print_alphapet - utilizeson the _putchar function to ptint alphapet a - z
-*/
-void print_alphabet(void);
-{
-	int ch;
 
-	for (ch = 'a'; ch < 'z'; ch++)
-	_putchar(ch);
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
 }

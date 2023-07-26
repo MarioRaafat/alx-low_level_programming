545ae1ea14a0936f8e4ef96510ef951b9ebf87ad
MarioRaafat <mariorafat10@gmail.com>
2023-07-26 05:52:03 -0700

    halfput

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: char to check
+ *
+ * Return: 0 is success
+ */
+void puts_half(char *str)
+{
+	int mid, n, nn, i;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+	}
+	fn = n - 1;
+	for (i = 0; i < fn; i++, fn--)
+	{
+	}
+	for (mid = i; mid < n; mid++)
+		_putchar(str[mid]);
+	_putchar('\n');
+}

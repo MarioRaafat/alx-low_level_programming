bf16539dd68ee61af927efa742b2e2146fe5db52
MarioRaafat <mariorafat10@gmail.com>
2023-07-26 06:18:52 -0700

    mm

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,8 +15,9 @@ void print_array(int *a, int n)
 		_putchar(a[i]);
 		if (i != n - 1)
 		{
-			_putchar(",");
-			_putchar(" ");
+			_putchar(char ",");
+			_putchar(char " ");
 		}
 	}
+	_putchar("\n");
 }

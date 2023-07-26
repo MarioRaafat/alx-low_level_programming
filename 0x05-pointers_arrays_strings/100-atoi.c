1d98c726ac10078cd46e164ab77485d0e62e850a
MarioRaafat <136023677+MarioRaafat@users.noreply.github.com>
2023-07-26 18:34:33 +0300

    advanceed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,9 +9,7 @@
  */
 int _atoi(char *s)
 {
-	int i, n, a, b = 0;
-
-	char xx[];
+	int i, n, a, b = 0, c = 0;
 
 	for (n = 0; s[n] != '\0'; n++)
 	{
@@ -20,6 +18,13 @@ int _atoi(char *s)
 	}
 	if (i % 2 == 1)
 		putchar(45);
+	for (a = 0; a < n; a++)
+	{
+		if (s[a] < 58 && s[a] > 47)
+			c++;
+	}
+	char xx[c];
+
 	for (a = 0; a < n; a++)
 	{
 		if (s[a] < 58 && s[a] > 47)

d77c6774d6b8fb63dd46df79754fc6fe5dab8994
MarioRaafat <136023677+MarioRaafat@users.noreply.github.com>
2023-07-26 17:39:39 +0300

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -17,13 +17,5 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-	for (d = 0; d < n; d++)
-	{
-		putchar(src[d]);
-	}
-	printf("\n");
-	for (d = 0; d < n; d++)
-		putchar(dest[d]);
-	printf("\n");
 	return (dest);
 }

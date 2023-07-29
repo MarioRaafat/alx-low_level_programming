394146d408e4205f1de8727f52fef7c8c604c61e
MarioRaafat <mariorafat10@gmail.com>
2023-07-28 18:48:25 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcmp - concat 2 string
+ * @dest:char
+ * @src:char
+ * Return:char
+ */
+char *_strcmp(char *dest, char *src)
+{
+	strcmp(dest, src);
+	return (dest);
+}

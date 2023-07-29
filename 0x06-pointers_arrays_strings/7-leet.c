6a52e94589e1aca92981dfb893f7c5f3ac484824
MarioRaafat <mariorafat10@gmail.com>
2023-07-29 07:46:51 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * leet - concat 2 string
+ * @m:char
+ * Return:char
+ */
+char *leet(char *m)
+{
+	int i;
+
+	for (i = 0; m[i] != '\0'; i++)
+	{
+		if (m[i] == 'a' || m[i] == 'A')
+		{
+			m[i] = '4';
+		}
+		if (m[i] == 'e' || m[i] == 'E')
+		{
+			m[i] = '3';
+		}
+		if (m[i] == 'o' || m[i] == 'O')
+		{
+			m[i] = '0';
+		}
+		if (m[i] == 'l' || m[i] == 'L')
+		{
+			m[i] = '1';
+		}
+		if (m[i] == 't' || m[i] == 'T')
+		{
+			m[i] = '7';
+		}
+	}
+	return (m);
+}

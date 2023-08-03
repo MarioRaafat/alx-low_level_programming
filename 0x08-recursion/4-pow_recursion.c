775703c8c95d5074e717dc79299a37c830d9cb54
MarioRaafat <mariorafat10@gmail.com>
2023-08-02 18:19:04 -0700

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,12 +8,9 @@
 
 int _pow_recursion(int x, int y)
 {
-	const int i = x;
 	if (y > 0)
 	{
-		if (y == 1)
-			return (x);
-		x *= _pow_recursion(x * i, y - 1);
+		return (x * _pow_recursion(x, y - 1));
 	}
 	if (y < 0)
 		return (-1);

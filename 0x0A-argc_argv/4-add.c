2b2cf5637e503f32133392d0fb9d9a7b3313f7a9
MarioRaafat <mariorafat10@gmail.com>
2023-08-04 18:49:19 -0700

    arg

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main (int argc, char **argv)
+{
+	int i, m, sum = 0, check;
+
+	for (check = 1; check < argc; check++)
+	{
+		m = atoi(argv[check]);
+		if (!(m > '0' && m <= '9'))
+			puts("Error");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		m = atoi(argv[i]);
+		sum += m;
+	}
+	return (0);
+}

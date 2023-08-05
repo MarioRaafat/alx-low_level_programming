bf7507a3d34100f4f88d1670d62558d9b0662bc4
MarioRaafat <mariorafat10@gmail.com>
2023-08-04 18:22:17 -0700

    arg

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf(%d\n, *argv[1] * *argv[2]);
+	}
+	else 
+		puts("Error");
+	return (0);
+}

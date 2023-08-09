bfa6c847b7c53d9e92e13dc63e97b2a989a63cf4
MarioRaafat <mariorafat10@gmail.com>
2023-08-09 10:02:01 -0700

    create

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,10 +18,13 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	arr = (char *) malloc(size * 4);
+	arr = (char *) malloc(size);
 	for (i = 0; i < size; i++)
 	{
 		*(arr + i) = putchar(c);
 	}
+	if (arr == 0)
+		return (NULL);
+	*(arr + i) = '\0';
 	return (arr);
 }

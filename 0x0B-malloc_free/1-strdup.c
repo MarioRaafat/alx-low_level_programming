ca958b884ce6088a80e8963950a7fcecbab06691
MarioRaafat <mariorafat10@gmail.com>
2023-08-09 10:22:47 -0700

    dup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,10 @@ char *_strdup(char *str)
 		return (NULL);
 	while (*(str + i))
 		i++;
-	arr = (char *) malloc(i - 1);
+	if (i == 1)
+		arr = (char *) malloc(i);
+	else
+		arr = (char *) malloc(i - 1);
 	for (d = 0; d < i; d++)
 		*(arr + d) = *(str + d);
 	*(arr + d) = '\0';

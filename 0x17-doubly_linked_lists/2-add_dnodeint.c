ae63d786e208d111b791dbbaf3e685918c266c4b
{your <mariorafat10@gmail.com>
2023-10-17 15:16:53 -0700

    task2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,9 +9,11 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *h = *head;
+	dlistint_t *new;
+	dlistint_t *h;
 
 	new = malloc(sizeof(dlistint_t));
+	h = *head;
 	while (h->prev)
 		h = h->prev;
 	if (!h)

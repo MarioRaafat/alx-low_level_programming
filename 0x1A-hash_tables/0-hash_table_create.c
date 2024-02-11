d445c2e9660b11ac5b845b0c0686ecaa3b064e33
MarioRaafat <mariorafat10@gmail.com>
2024-02-11 07:51:01 -0800

    task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (arr->array == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 		arr->array[i] = NULL;
 
 	return arr;

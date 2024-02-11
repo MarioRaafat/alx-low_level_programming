3de24e296f4c09b28c02cc92bf458e0640af0726
MarioRaafat <mariorafat10@gmail.com>
2024-02-11 09:35:08 -0800

    Gameed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -32,7 +32,7 @@ int hash_table_set(hash_table_t* ht, const char* key, const char* value)
 		}
 	}
 
-	hash_node = malloc(sizeof(hash_node_t))
+	hash_node = malloc(sizeof(hash_node_t));
 	hash_node->key = strdup(key);
 	hash_node->value = strdup(value);
 

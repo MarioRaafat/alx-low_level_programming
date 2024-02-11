6fb2df1edad4d1ac943aaf50f625c57e9bb6de9c
MarioRaafat <mariorafat10@gmail.com>
2024-02-11 07:52:45 -0800

    task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,15 +9,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-		hash_table_t* arr = malloc(sizeof(hash_table_t));
+	hash_table_t *ht;
+	unsigned long int i;
 
-	arr->size = size;
-	arr->array = malloc(sizeof(hash_node_t *) * size);
-	if (arr->array == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	for (int i = 0; i < size; i++)
-		arr->array[i] = NULL;
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-	return arr;
+	return (ht);
 }

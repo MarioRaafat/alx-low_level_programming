46a8a7d4e6f20ccc449051223961ed86e1adb1fc
MarioRaafat <mariorafat10@gmail.com>
2024-06-22 13:29:44 -0700

    IDK

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,37 +1,64 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - searches for a value in a sorted array of integers
- * using the Binary search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
- * @value: value to search for
+ * rec_search - searches for a value in an array of
+ * integers using the Binary search algorithm
  *
- * Return: the index where value is located, or -1
- * if value is not present in array or if array is NULL
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
+int rec_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
 
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+	{
+		if (half > 0)
+			return (rec_search(array, half + 1, value));
+		return ((int)half);
+	}
+
+	if (value < array[half])
+		return (rec_search(array, half + 1, value));
+
+	half++;
+	return (rec_search(array + half, size - half, value) + half);
+}
+
+/**
+ * advanced_binary - calls to rec_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
 int advanced_binary(int *array, size_t size, int value)
 {
-    size_t i, low, high;
+	int index;
 
-    if (array == NULL)
-        return (-1);
-    low = 0;
-    high = size - 1;
-    while (low <= high)
-    {
-        printf("Searching in array: ");
-        for (i = low; i < high; i++)
-            printf("%d, ", array[i]);
-        printf("%d\n", array[i]);
-        i = low + (high - low) / 2;
-        if (array[i] == value)
-            return (i);
-        if (array[i] < value)
-            low = i + 1;
-        else
-            high = i;
-    }
-    return (-1);
+	index = rec_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
 }
